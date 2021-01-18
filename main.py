6ceeff16d95ea835ea100bb30494256fd4c9ac20
HelenePiet <helene.m.piet@gmail.com>
2021-01-17 22:18:22 -0700

    Creation of main.py and Expense.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from Expense import *
+
+first_expense = Expense(name='Home Depot', category='Home Improvement', date='today', recurrency=False,
+                        price=47.56)
+
+print(first_expense.name)
